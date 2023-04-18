2b20659a4dce3b5b934e9e577f93a12347294aa6
unknown <roberttabak78@gmail.com>
2023-04-18 20:10:04 +0200

    first commit

---
+++ 00_intro/bmi.py
@@ -0,0 +1,4 @@
+w = float(input("podaj wage->"))
+h = float(input("podaj wzrost->"))
+bmi = w / (h ** 2)
+print("Your BMI is:", round(bmi, 2))
---
+++ 00_intro/weeks.py
@@ -0,0 +1,10 @@
+x=60*24*7*7
+print(x)
+
+
+
+print(60*24*7*7)
+
+lerning_time=75
+my_time=3
+print ("Do nauczenia sie nowej umiejetnosci potrzebuje ok", lerning_time //my_time, "tygodni")
