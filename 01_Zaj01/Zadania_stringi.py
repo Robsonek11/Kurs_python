918deca5d27897405ef9e5b9a40baba620d9cdee
unknown <roberttabak78@gmail.com>
2023-04-24 22:22:28 +0200

    zadania

--- 01_Zaj01/Zadania_stringi.py
+++ 01_Zaj01/Zadania_stringi.py
@@ -13,7 +13,11 @@ print(quote[:lenght])
 print(quote[-2:-1])
 print(quote[lenght::3])
 print(quote[1:53:2])
-print()
+print(quote.split())
+print(" {} {} {} {} {} {} {} {} {} {} ".format('Honesty', 'is', 'the', 'first', 'chapter', 'in', 'the', 'book', 'of', 'wisdom.'))
+print(" {9} {8} {7} {6} {5} {4} {3} {2} {1} {0}".format('Honesty', 'is', 'the', 'first', 'chapter', 'in', 'the', 'book', 'of', 'wisdom.'))
+
+#print(wyświetl cąły tekst odwrotnie)
 print(quote.replace('wisdom', 'friendship'))
 #Zad4
 book = str(input('Podaj tytuł ksiązki? '))
@@ -27,4 +31,14 @@ txt = 'book'
 print(" ".join(txt))
 print(len(txt))
 #Zad5
+tekst = str(input('Podaj tekst? '))
+
+
 #Zad6
+txt = "Beautiful is better than ugly.\n Explicit is better than implicit.Simple is better than complex.\n Complex is better than complicated. Flat is better than nested. \nSparse is better than dense. \nReadability counts. Special cases aren\'t special enough to break the rules.Although practicality beats purity. \nErrors should never pass silently. \nUnless explicitly silenced. In the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never. Although never is often better than *right* now. If the implementation is hard to explain, it\'s a bad idea.\nIf the implementation is easy to explain, it may be a good idea.Namespaces are one honking great idea -- let\"s do more of those!"
+print(txt.count("better"))
+print(txt.replace('*', ' '))
+print(txt.count("explain"))
+print(txt.replace('explain', 'understand', 1))
+print(txt.replace(' ', '-'))
+txt.replace(' ', '.', ))
