f7e3dfac863f8ce52560fe8c5e9b81ad35c8d13d
unknown <roberttabak78@gmail.com>
2023-04-24 21:27:12 +0200

    zadania

---
+++ 01_Zaj01/Zadanie_bool.py
@@ -0,0 +1,18 @@
+#Zad2
+a = 27
+b = 7
+c = a//b
+print('Reszta z dzielenia 29 przez 7 = ', c)
+print ('czy reszta z dzielenia bez reszty 29 przez 7 wynosi 5', c==5 )
+
+#Zad4
+liczba = int(input('Podaj liczbę? '))
+print ( 'wynik', liczba / 2)
+print('Liczba jest parzysta', liczba % 2 == 0 < True)
+
+#Zad6
+x = 129
+y = 17
+z = x//y
+print('Reszta z dzielenia 129 przez 17 = ', z)
+print ('czy reszta z dzielenia bez reszty 129 przez 17 wynosi 3', z==3 )
