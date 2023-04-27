0f20c7af57e8f8579c435a6a6db3cb6a27cf3dfd
unknown <roberttabak78@gmail.com>
2023-04-27 15:55:31 +0200

    zadania domowe_zadania_war

---
+++ 02_Zaj02/petle.py
@@ -0,0 +1,9 @@
+items = ["latarka", "spiwór", "woda", "telefon"]
+
+for i in items:
+    print(i)
+
+a=0
+for i in range(1,11,1):
+    a = a + i
+    print(a)
---
+++ 02_Zaj02/while.py
@@ -0,0 +1,15 @@
+f = 200
+while f > 0:
+    c=5/9*(f-32)
+    print('Temperatura w stopniach Fahernheit',f)
+    print('Temperatura w stopniach Fahernheit',c)
+    f=f-20
+
+print('\nNo nie, znowu trzeba kupić ciastka!')
+text = 'herbata'
+
+for i in range(20, 220, 20):
+    c = 5 / 9 * (i - 32)
+    print('Temperatura w stopniach Fahernheit', i)
+    print('Temperatura w stopniach Fahernheit', c)
+   
